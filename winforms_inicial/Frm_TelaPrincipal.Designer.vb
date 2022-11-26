00bf0de4f7ce45a99ca378938f8efd975a022353
Rafael Ledo <51183852+rafaelledo@users.noreply.github.com>
2022-11-26 18:02:55 -0300

    projeto 2

---
+++ winforms_inicial/Frm_TelaPrincipal.Designer.vb
@@ -0,0 +1,51 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Frm_TelaPrincipal
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Btn_Principal = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Btn_Principal
+        '
+        Me.Btn_Principal.Location = New System.Drawing.Point(345, 358)
+        Me.Btn_Principal.Name = "Btn_Principal"
+        Me.Btn_Principal.Size = New System.Drawing.Size(94, 29)
+        Me.Btn_Principal.TabIndex = 0
+        Me.Btn_Principal.Text = "Click Here"
+        Me.Btn_Principal.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Btn_Principal)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Aplicação Olá Mundo"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Btn_Principal As Button
+End Class
---
+++ winforms_inicial/Frm_TelaPrincipal.vb
@@ -0,0 +1,9 @@
+﻿Public Class Frm_TelaPrincipal
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Btn_Principal_Click(sender As Object, e As EventArgs) Handles Btn_Principal.Click
+        MsgBox("Olá Mundo!", MsgBoxStyle.Critical, "Erro")
+    End Sub
+End Class
