ec81bc4abf8bdf60dd6dd24ec07c63f771b23b9e
Rafael Ledo <rafaeloliveiraledo@gmail.com>
2022-11-28 00:54:56 -0300

    update p5_ControleComLacos

---
+++ p5_ControleComLacos/Frm_TelaPrincipal_05.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+    End Sub
+
+    Private Sub Btn_Principal_Click(sender As Object, e As EventArgs) Handles Btn_Principal.Click
+        Dim investimento As Decimal = Val(Txt_Principal.Text)
+        Dim rendimento As Decimal = Val(Txt_Rendimento.Text) / 100
+        Dim periodos As Integer = Val(Txt_Periodos.Text)
+        Dim contador As Integer = 1
+
+        While (contador <= periodos)
+            investimento += investimento * rendimento
+            contador += 1
+        End While
+
+
+        MsgBox("O saldo do investimento após " + periodos.ToString + " meses é " + investimento.ToString)
+    End Sub
+End Class
